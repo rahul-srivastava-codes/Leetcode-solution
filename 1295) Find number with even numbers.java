1a590c0b5536f449683b9b214be40b137dd63f1e
Rahul Srivastava <117383410+rahul-srivastava-codes@users.noreply.github.com>
2025-03-15 11:07:45 +0530

    Update 1295) Find number with even numbers.java

--- 1295) Find number with even numbers.java
+++ 1295) Find number with even numbers.java
@@ -19,3 +19,34 @@ class Solution {
 }
 
 
+
+// more genuine solution, convert it into string and then find the length
+class Solution {
+    public int findNumbers(int[] nums) {        
+        // int count = 0;
+        // for(int i = 0; i< nums.length; i++){
+        //     int x = 0;
+        //     while(nums[i]>0){
+        //         nums[i] = nums[i]/10;
+        //         x++;                
+        //     }
+        //     if((x%2)==0){
+        //         count++;
+        //     }
+        // }
+        // return count;
+        // for(int i = 0; i< nums.length; i++){
+
+        // }
+        int count = 0;
+        for(int i = 0; i< nums.length; i++){
+            int x = Integer.toString(nums[i]).length();
+
+            if((x%2)==0){
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
