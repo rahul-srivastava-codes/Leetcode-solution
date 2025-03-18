dbd79369949249f489ade043c51e97139a89ceac
Rahul Srivastava <117383410+rahul-srivastava-codes@users.noreply.github.com>
2025-03-18 23:59:49 +0530

    Create 852) Peak index of element.java

---
+++ 852) Peak index of element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {        
+        // using linear search
+        // int max =arr[0];
+        // int count = 0;
+        // for(int i=1; i<arr.length; i++){
+        //     if(arr[i]> max){
+        //         max = arr[i];
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+    }
+}
+
+// You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+// Return the index of the peak element.
+// Your task is to solve it in O(log(n)) time complexity.
