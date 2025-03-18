f28defbf22d432d523e9c4ace84a3c20280ccedc
Rahul Srivastava <117383410+rahul-srivastava-codes@users.noreply.github.com>
2025-03-19 00:09:44 +0530

    Update 852) Peak index of element.java

--- 852) Peak index of element.java
+++ 852) Peak index of element.java
@@ -17,3 +17,19 @@ class Solution {
 // You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
 // Return the index of the peak element.
 // Your task is to solve it in O(log(n)) time complexity.
+
+
+// using binary search
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {       
+        int start = 0;
+        int end = arr.length -1;
+        while(start<end){
+            int mid = (start + end)/2;
+            if(arr[mid]>arr[mid+1]) end = mid;
+             else  start = mid+1;                   
+        }
+        System.gc();
+        return start;
+    }
+}
